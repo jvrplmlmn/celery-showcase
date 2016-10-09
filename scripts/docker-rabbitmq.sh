02ab74aadf066c6e60a9c8b5f19517c18de3e23a
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2016-10-09 16:37:10 +0200

    Run RabbitMQ with the management plugin enabled so Flower can inspect the Broker

--- scripts/docker-rabbitmq.sh
+++ scripts/docker-rabbitmq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -e RABBITMQ_PASS=guest -e RABBITMQ_USER=guest -p 5672:5672 rabbitmq
+docker run -e RABBITMQ_PASS=guest -e RABBITMQ_USER=guest -p 5672:5672 -p 15672:15672 rabbitmq:management
